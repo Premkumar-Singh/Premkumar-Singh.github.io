a903576efe30a97ba4356c94603703d410cc053b
Premkumar-Singh <premkumarrsingh6@gmail.com>
2025-06-30 18:11:47 +0530

    Initial website upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("Hello! This is a GitHub Pages site.");
+}
+
+function toggleMenu() {
+  const nav = document.getElementById('nav-links');
+  nav.classList.toggle('show');
+}
